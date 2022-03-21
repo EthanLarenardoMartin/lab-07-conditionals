322a7011f47003c01a2c0d73b3b54b55228e8399
EthanLarenardoMartin <ethan.martin@mail.citytech.cuny.edu>
2022-03-21 15:54:39 -0400

    +cake.py

---
+++ cake.py
@@ -0,0 +1,8 @@
+cakeorbust = input("Do you want cake? (yes/no) ")
+
+if cakeorbust == "yes":
+    print ("Have some cake")
+elif cakeorbust == "no":
+    print ("No Cake for you!")
+else:
+    print("Sorry, I don't understand your response...")
